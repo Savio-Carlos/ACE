8dd2779c07b809ff72e1256ff40a9e6609062a7d
Savio-Carlos <carlossavio03@gmail.com>
2026-05-13 18:32:55 -0300

    h mexico 2026

--- GYM/mexico/2026/1fecha/h.cpp
+++ GYM/mexico/2026/1fecha/h.cpp
@@ -1,57 +1,206 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAXN = 1000005;
-char A[MAXN], B[MAXN];
-int X[MAXN], d1[MAXN], d2[MAXN];
-int fDiff[MAXN+2], gDiff[MAXN+2], fArr[MAXN], gArr[MAXN];
 
-int main() {
-    int T; scanf("%d", &T);
-    while (T--) {
-        scanf("%s", A); scanf("%s", B);
-        int n = strlen(A);
-        int prefL = 0, sufL = 0;
-        while (prefL < n && A[prefL] == B[n-1-prefL]) prefL++;
-        while (sufL < n && A[n-1-sufL] == B[sufL]) sufL++;
-        int muStar = min(prefL, sufL);
-        for (int i = 0; i < n; i++) X[i] = (A[i]-'a')*26 + (B[i]-'a');
-        for (int i = 0; i <= n+1; i++) fDiff[i] = gDiff[i] = 0;
-        // Manacher odd
-        { int l=0,r=-1;
-          for (int i = 0; i < n; i++) {
-            int k = (i>r) ? 1 : min(d1[l+r-i], r-i+1);
-            while (i-k>=0 && i+k<n && X[i-k]==X[i+k]) k++;
-            d1[i] = k;
-            if (i+k-1>r) { l=i-k+1; r=i+k-1; }
-        }}
-        // Manacher even
-        { int l=0,r=-1;
-          for (int i = 0; i < n; i++) {
-            int k = (i>r) ? 0 : min(d2[l+r-i+1], r-i+1);
-            while (i-k-1>=0 && i+k<n && X[i-k-1]==X[i+k]) k++;
-            d2[i] = k;
-            if (i+k-1>r) { l=i-k; r=i+k-1; }
-        }}
-        for (int i = 0; i < n; i++) {
-            int K = d1[i];
-            fDiff[i-K+1]++; fDiff[i+1]--;
-            gDiff[i]++; gDiff[i+K]--;
-            K = d2[i];
-            if (K > 0) {
-                fDiff[i-K]++; fDiff[i]--;
-                gDiff[i]++; gDiff[i+K]--;
-            }
-        }
-        int cur = 0;
-        for (int i = 0; i < n; i++) { cur += fDiff[i]; fArr[i] = cur; }
-        cur = 0;
-        for (int i = 0; i < n; i++) { cur += gDiff[i]; gArr[i] = cur; }
-        long long total = muStar;
-        for (int mu = 1; mu <= muStar; mu++) {
-            if (mu < n) total += fArr[mu];
-            if (n-mu-1 >= 0) total += gArr[n-mu-1];
-        }
-        printf("%lld\n", total);
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define endl '\n'
+#define int long long
+#define ld long double
+
+namespace dbg {
+    constexpr const char* RESET      = "\033[0m";
+    constexpr const char* BOLD_BLUE  = "\033[1;34m";
+    constexpr const char* YELLOW     = "\033[33m";
+    constexpr const char* BOLD_WHITE = "\033[1;37m";
+
+    template<typename T, typename = void> struct is_container : false_type {};
+    template<typename T> struct is_container<T, void_t<typename T::value_type>> : bool_constant<!is_same_v<T, string> && !is_same_v<T, string_view>> {};
+
+    inline ostream& print_atom(ostream& os, bool b)         { return os << (b ? "true" : "false"); }
+    inline ostream& print_atom(ostream& os, char c)         { return os << '\'' << c << '\''; }
+    inline ostream& print_atom(ostream& os, const string& s){ return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, string_view s)  { return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, const char* s)  { return os << '"' << s << '"'; }
+
+    template<typename T, enable_if_t<!is_container<T>::value, int> = 0>
+    ostream& print_atom(ostream& os, const T& x) { return os << x; }
+
+    template<typename T1, typename T2>
+    ostream& print_atom(ostream& os, const pair<T1, T2>& p) {
+        os << '{'; print_atom(os, p.first); os << ", "; print_atom(os, p.second); return os << '}';
     }
-    return 0;
+    template<typename... T>
+    ostream& print_atom(ostream& os, const tuple<T...>& t) {
+        os << '{';
+        apply([&os](auto const&... args) {
+            size_t n = 0;
+            ((print_atom(os, args), os << (++n != sizeof...(T) ? ", " : "")), ...);
+        }, t);
+        return os << '}';
+    }
+
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, stack<T, C> s) {
+        os << '{'; bool f = true; while (!s.empty()) { if (!f) os << ", "; print_atom(os, s.top()); s.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, queue<T, C> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.front()); q.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C, typename Cmp>
+    ostream& print_atom(ostream& os, priority_queue<T, C, Cmp> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.top()); q.pop(); f = false; } return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& print_atom(ostream& os, const C& v) {
+        os << '{'; bool f = true;
+        for (const auto& x : v) { if (!f) os << ", "; print_atom(os, x); f = false; }
+        return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& operator<<(ostream& os, const C& v)            { return print_atom(os, v); }
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return print_atom(os, p); }
+    template<typename... T>
+    ostream& operator<<(ostream& os, const tuple<T...>& t)  { return print_atom(os, t); }
+
+    inline size_t split_arg(string_view s) {
+        int depth = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '"' || c == '\'') {
+                char q = c; ++i;
+                while (i < s.size() && s[i] != q) { if (s[i] == '\\' && i + 1 < s.size()) ++i; ++i; }
+            } else if (c == '(' || c == '[' || c == '{' || c == '<') ++depth;
+            else if (c == ')' || c == ']' || c == '}' || c == '>') --depth;
+            else if (c == ',' && depth == 0) return i;
+        }
+        return string_view::npos;
+    }
+
+    inline void debug_out(string_view) { cerr << '\n' << flush; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, const H& h, const T&... t) {
+        size_t cpos = split_arg(s);
+        auto name = s.substr(0, cpos);
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t')) name.remove_prefix(1);
+        while (!name.empty() && (name.back()  == ' ' || name.back()  == '\t')) name.remove_suffix(1);
+        cerr << YELLOW << name << RESET << " = " << BOLD_WHITE;
+        print_atom(cerr, h) << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            debug_out(cpos == string_view::npos ? string_view{} : s.substr(cpos + 1), t...);
+        } 
+        else cerr << '\n' << flush;
+    }
+}
+using namespace dbg;
+
+// #define DEBUG
+
+#if defined(DEBUG)
+    #define winton (void)0
+    #define debug(...) do { cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "] " << RESET; dbg::debug_out(#__VA_ARGS__, __VA_ARGS__); } while (0)
+#else
+    #define winton ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) ((void)0)
+#endif
+
+vector<int> manacher_odd(string s) {
+    int n = s.size();
+    s = "$" + s + "^";
+    vector<int> p(n + 2);
+    int l = 0, r = 1;
+    for(int i = 1; i <= n; i++) {
+        p[i] = min(r - i, p[l + (r - i)]);
+        while(s[i - p[i]] == s[i + p[i]]) {
+            p[i]++;
+        }
+        if(i + p[i] > r) {
+            l = i - p[i];
+            r = i + p[i];
+        }
+    }
+    return vector<int>(begin(p) + 1, end(p) - 1);
+}
+
+vector<int> manacher(string s) {
+    string t;
+    for(auto c: s) {
+        t += string("#") + c;
+    }
+    auto res = manacher_odd(t + "#");
+    return res;
+}
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    int n = a.size();
+
+    int mx = 0;
+    for(int i = 0; i < n; i++) {
+        if(a[i] == b[n-1-i] && b[i] == a[n-1-i]) mx = i + 1;
+        else break;
+    }
+
+    auto mnc_a = manacher(a);
+    auto mnc_b = manacher(b);
+    debug(mnc_a);
+    debug(mnc_b);
+
+    int cnt1 = 0;
+    int cnt2 = 0;
+
+    // impares
+    for(int i = 0; i < n; i++) {
+        int ra = (mnc_a[2*i + 1] - 2) / 2;
+        int rb = (mnc_b[2*i + 1] - 2) / 2;
+        int raio = min(ra, rb);
+
+        debug(ra,rb,raio,i);
+        int l = max<int>(0, i - mx);
+        int r = min(raio, i - 1);
+        if(r >= l) cnt1 += r - l + 1;
+        debug(l,r);
+        
+        l = max<int>(0, n - 1 - mx - i);
+        r = min(raio, n - 2 - i);
+        if(r >= l) cnt2 += r - l + 1;
+        debug(l,r);
+        
+        debug(i,cnt1,cnt2);
+    }
+
+    // pares
+    for(int i = 0; i < n - 1; i++) {
+        int ra = (mnc_a[2*i + 2] - 1) / 2;
+        int rb = (mnc_b[2*i + 2] - 1) / 2;
+        int raio = min(ra, rb);
+        debug(ra, mnc_a[2*i+2]);
+        int l = max<int>(1, i - mx + 1);
+        int r = min(raio, i);
+        if(r >= l) cnt1 += r - l + 1;
+        debug(i, l, r);
+
+        l = max<int>(1, n - 1 - mx - i);
+        r = min(raio, n - 2 - i);
+        debug(l,r);
+        
+        if(r >= l) cnt2 += r - l + 1;
+        debug(cnt2);    
+    }
+    
+
+    cout << mx + cnt1 + cnt2 << endl;
+}
+
+signed main(){
+    winton;
+    int t;
+    cin >> t;
+    while(t--) solve();
+
 }
