63aecd480876863a1709571367f62287a67bf147
Carlos <carlossavio03@gmail.com>
2026-05-26 16:33:25 -0300

    boliviaaar

--- GYM/bolivia/2025/a.cpp
+++ GYM/bolivia/2025/a.cpp
@@ -1,12 +1,118 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// #define int long long
-#define ld long double
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define endl '\n'
-#define fastio ios_base::sync_with_stdio(0),cin.tie(0)
+#define int long long
+#define ld long double
+
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U>& p) { return is >> p.first >> p.second; }
+template<typename... T> istream& operator>>(istream& is, tuple<T...>& t) { apply([&is](auto&... args) { ((is >> args), ...); }, t); return is; }
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (auto& x : v) is >> x; return is; }
+template<typename T, size_t N> istream& operator>>(istream& is, T (&arr)[N]) { for (auto& x : arr) is >> x; return is; }
+template<size_t N> istream& operator>>(istream& is, array<int, N>& arr) { for (auto& x : arr) is >> x; return is; }
+
+namespace dbg {
+    constexpr const char* RESET      = "\033[0m";
+    constexpr const char* BOLD_BLUE  = "\033[1;34m";
+    constexpr const char* YELLOW     = "\033[33m";
+    constexpr const char* BOLD_WHITE = "\033[1;37m";
+
+    template<typename T, typename = void> struct is_container : false_type {};
+    template<typename T> struct is_container<T, void_t<typename T::value_type>> : bool_constant<!is_same_v<T, string> && !is_same_v<T, string_view>> {};
+
+    inline ostream& print_atom(ostream& os, bool b)         { return os << (b ? "true" : "false"); }
+    inline ostream& print_atom(ostream& os, char c)         { return os << '\'' << c << '\''; }
+    inline ostream& print_atom(ostream& os, const string& s){ return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, string_view s)  { return os << '"' << s << '"'; }
+    inline ostream& print_atom(ostream& os, const char* s)  { return os << '"' << s << '"'; }
+
+    template<typename T, enable_if_t<!is_container<T>::value, int> = 0>
+    ostream& print_atom(ostream& os, const T& x) { return os << x; }
+
+    template<typename T1, typename T2>
+    ostream& print_atom(ostream& os, const pair<T1, T2>& p) {
+        os << '{'; print_atom(os, p.first); os << ", "; print_atom(os, p.second); return os << '}';
+    }
+    template<typename... T>
+    ostream& print_atom(ostream& os, const tuple<T...>& t) {
+        os << '{';
+        apply([&os](auto const&... args) {
+            size_t n = 0;
+            ((print_atom(os, args), os << (++n != sizeof...(T) ? ", " : "")), ...);
+        }, t);
+        return os << '}';
+    }
+
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, stack<T, C> s) {
+        os << '{'; bool f = true; while (!s.empty()) { if (!f) os << ", "; print_atom(os, s.top()); s.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C>
+    ostream& print_atom(ostream& os, queue<T, C> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.front()); q.pop(); f = false; } return os << '}';
+    }
+    template<typename T, typename C, typename Cmp>
+    ostream& print_atom(ostream& os, priority_queue<T, C, Cmp> q) {
+        os << '{'; bool f = true; while (!q.empty()) { if (!f) os << ", "; print_atom(os, q.top()); q.pop(); f = false; } return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& print_atom(ostream& os, const C& v) {
+        os << '{'; bool f = true;
+        for (const auto& x : v) { if (!f) os << ", "; print_atom(os, x); f = false; }
+        return os << '}';
+    }
+
+    template<typename C, enable_if_t<is_container<C>::value, int> = 0>
+    ostream& operator<<(ostream& os, const C& v)            { return print_atom(os, v); }
+    template<typename T1, typename T2>
+    ostream& operator<<(ostream& os, const pair<T1, T2>& p) { return print_atom(os, p); }
+    template<typename... T>
+    ostream& operator<<(ostream& os, const tuple<T...>& t)  { return print_atom(os, t); }
+
+    inline size_t split_arg(string_view s) {
+        int depth = 0;
+        for (size_t i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '"' || c == '\'') {
+                char q = c; ++i;
+                while (i < s.size() && s[i] != q) { if (s[i] == '\\' && i + 1 < s.size()) ++i; ++i; }
+            } else if (c == '(' || c == '[' || c == '{' || c == '<') ++depth;
+            else if (c == ')' || c == ']' || c == '}' || c == '>') --depth;
+            else if (c == ',' && depth == 0) return i;
+        }
+        return string_view::npos;
+    }
+
+    inline void debug_out(string_view) { cerr << '\n' << flush; }
+    template<typename H, typename... T>
+    void debug_out(string_view s, const H& h, const T&... t) {
+        size_t cpos = split_arg(s);
+        auto name = s.substr(0, cpos);
+        while (!name.empty() && (name.front() == ' ' || name.front() == '\t')) name.remove_prefix(1);
+        while (!name.empty() && (name.back()  == ' ' || name.back()  == '\t')) name.remove_suffix(1);
+        cerr << YELLOW << name << RESET << " = " << BOLD_WHITE;
+        print_atom(cerr, h) << RESET;
+        if constexpr (sizeof...(t) > 0) {
+            cerr << ", ";
+            debug_out(cpos == string_view::npos ? string_view{} : s.substr(cpos + 1), t...);
+        } 
+        else cerr << '\n' << flush;
+    }
+}
+using namespace dbg;
+
+#define DEBUG
+
+#if defined(DEBUG)
+    #define fastio (void)0
+    #define debug(...) do { cerr << BOLD_BLUE << "[" << __func__ << ":" << __LINE__ << "] " << RESET; dbg::debug_out(#__VA_ARGS__, __VA_ARGS__); } while (0)
+#else
+    #define fastio ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+    #define debug(...) ((void)0)
+#endif
 
 
 struct frac {
@@ -27,32 +133,60 @@ struct frac {
         return {a*r.num, r.den};
     }
 
-        friend frac operator+(const frac& l, const frac& r){
+    friend frac operator+(const frac& l, const frac& r){
         return {l.num*r.den + l.den*r.num, l.den*r.den};
     }
 
     friend frac operator/(const frac& l, const frac& r){
         return {l.num*r.den, l.den*r.num};
     }
+    void frac_abs(){
+        num = abs(num);
+        den = abs(den);
+    }
+
+    friend ostream& operator<<(ostream& out, frac f) {
+		out << f.num << '/' << f.den;
+		return out;
+	}
 };
 
 int A, B, C, L, R;
 
 const int N = 3*100;
+
 frac f(frac x){
-    frac r(A*x*x + B*x + C);
-    if (r.num < 0) r.num *= -1;
+    frac r(A*x.num*x.num + B*x.num + C, x.den);
+    r.frac_abs();
     return r;
 }
 
 frac integrate(int a, int b){
     frac s;
-    frac h(b-a, N);
-    for(int i = 0; i < N; i++){
-        s = s + f(a + i*h)*(i%3 ? 3 : 2);
+    frac h(a-b,N);
+    h.frac_abs();
+
+    for (int i = 1; i < N; i++){
+        frac x(i*h);
+        x = x + a;
+        x.frac_abs();
+        frac som = f(x);
+        som.frac_abs();
+        som = som * (i%3 ? 3 : 2);
+        s = s + som;
     }
-    frac aa = ((f(a) + s + f(b))*3*h)/8;
-    return aa;
+    frac aa(a);
+    frac bb(b);
+    aa.frac_abs();
+    bb.frac_abs();
+    frac r(f(aa) + s + f(bb));
+    r.frac_abs();
+    r = r*h;
+    r = r*3;
+    r = r/8;
+    r.frac_abs();
+    debug(s,r,h,aa,bb);
+    return r; 
 }
 
 signed main(){
@@ -63,7 +197,6 @@ signed main(){
         // int a, b, c, l, r;
         cin >> A >> B >> C >> L >> R;
         frac res = integrate(L,R);
-
         cout << res.num << "/" << res.den << endl;;
     }
 
