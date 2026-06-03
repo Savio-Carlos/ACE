6c5a00a6189588aa85ea8033e55e3fa76caa2f76
Savio-Carlos <carlossavio03@gmail.com>
2026-06-02 23:35:55 -0300

    a

--- RESOURCES/Biblioteca/Codigo/Extra/Template.cpp
+++ RESOURCES/Biblioteca/Codigo/Extra/Template.cpp
@@ -12,38 +12,7 @@ using namespace std;
 #define debugv(x) cerr << #x << ": "; for(auto i : x) cerr << i << " "; cerr << endl
 #define debugp(x) cerr << #x << ": " << x.first << " " << x.second << endl
 
-namespace dbg {
-    template<class A, class B> ostream& operator<<(ostream& o, const pair<A,B>& p) {
-        return o << '(' << p.first << ", " << p.second << ')';
-    }
-    template<class T, class = enable_if_t<!is_same_v<T,string> && !is_same_v<T,string_view>>>
-    auto operator<<(ostream& o, const T& v) -> decltype(v.begin(), o) {
-        o << '{'; bool f = 1;
-        for (auto& x : v) { o << (f ? "" : ", ") << x; f = 0; }
-        return o << '}';
-    }
-    void out(string_view) { cerr << '\n'; }
-    template<class H, class... T> void out(string_view s, H h, T... t) {
-        auto c = s.find(',');
-        cerr << s.substr(0, c) << " = " << h;
-        if constexpr (sizeof...(t)) {
-            cerr << " | ";
-            out(s.substr(s.find_first_not_of(" ,", c)), t...);
-        } 
-        else cerr << '\n';
-    }
-}
-using namespace dbg;
-
-#define DEBUG
-
-#ifdef DEBUG
-    #define fastio (void)0
-    #define debug(...) cerr << "[" << __LINE__ << "] ", out(#__VA_ARGS__, __VA_ARGS__)
-#else
-    #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL)
-    #define debug(...) (void)0
-#endif
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL)
 
 signed main(){
     fastio;
