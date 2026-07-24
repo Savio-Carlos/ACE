2cb4d28e1c8c6729ab7c17a68e0d3116b103a5ae
Savio-Carlos <carlossavio03@gmail.com>
2026-07-24 17:27:51 -0300

    b subreg 2018

--- gym/subreg/2018/b.cpp
+++ gym/subreg/2018/b.cpp
@@ -1,11 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+#define int long long
 #define fastio ios_base::sync_with_stdio(false), cin.tie(NULL)
-#define endl '\n'
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
 
-int main(){
+const int MAX = 105;
+
+signed main(){
     fastio;
+    
+    vector<vector<int>> grid(MAX+1, vector<int>(MAX+1,0));
+    for (int i = 1; i <= MAX; i++){
+        for (int j = 1; j <= MAX; j++){
+            if (i == j) continue;
+            int mex = 0;
+            set<int> s;
+            
+            for (int r = i-1; r; r--){
+                if (r == j) continue;
+                s.insert(grid[r][j]);
+            }
+            
+            for (int c = j-1; c; c--){
+                if (c == i) continue;
+                s.insert(grid[i][c]);
+            }
+            
+            for (int r = i-1, c = j-1; r and c; r--, c--){
+                s.insert(grid[r][c]);
+            }
+
+            while(s.find(mex) != s.end())mex++;
+            grid[i][j] = mex;
+        }
+    }
+    
     int n;
     cin >> n;
     bool can_in_one = false;
@@ -15,16 +45,10 @@ int main(){
         cin >> x >> y;
         if (!x or !y or x == y){
             can_in_one = 1;
-            continue;
+            break;
         } 
         
-        int up = (y >= 2 ? abs(y-2) + abs(x-1) : 0); 
-        int left = (x >= 2 ? abs(x-2) + abs(y-1) : 0);
-        if (x < y) left--;
-        else up--; 
-        
-        xr ^= max(left, up);
-        // cout << xr << " " << up << " " << left << endl;
+        xr ^= grid[x][y];
     }   
 
     if (can_in_one or xr) cout << "Y" << endl;
